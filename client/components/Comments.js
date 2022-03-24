fa05b65d81c5f9deaf33c11671becfcbedb8da66
jer_ng <coh.jr11@gmail.com>
2022-03-24 15:32:44 -0700

    improve base nested comment rendering

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,5 +1,4 @@
 import * as objTraverse from 'obj-traverse/lib/obj-traverse';
-import { Button } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import DisplayComments from './DisplayComments';
 // display 2 latest comments
@@ -24,33 +23,16 @@ const Comments = ({ post }) => {
   };
   useEffect(() => {
     if (!replies || !comments) return;
-
-    for (let i = 0; i < comments.length; i++) {
-      testFormmated.push(comments[i]);
-    }
-    for (let i = 0; i < testFormmated.length; i++) {
+    for (let i = 0; i < comments.length; i++) testFormmated.push(comments[i]);
+    for (let i = 0; i < testFormmated.length; i++)
       testFormmated[i] = recursiveBuildComment(testFormmated[i]);
-    }
+    setFormattedComments(testFormmated);
   }, [comments, replies, post]);
   return (
     <div>
-      {comments.map((each, index) => {
-        return (
-          <DisplayComments
-            comment={each}
-            key={index}
-            post={post}
-            replies={replies}
-            comments={comments}
-          />
-        );
+      {formattedComments.map((each, index) => {
+        return <DisplayComments comment={each} key={index} post={post} comments={comments} />;
       })}
-      <Button onClick={test}>Test</Button>
-      <div>
-        {/* {JSON.stringify(comments, null, 2)}
-        <hr />
-        {JSON.stringify(replies, null, 2)} */}
-      </div>
       {/* {comments.length - next > 0 ? (
         <Button onClick={() => setNext((prev) => prev + 10)}> See more comments</Button>
       ) : (
--- client/components/DisplayComments.js
+++ client/components/DisplayComments.js
@@ -1,30 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import Comment from './Comment';
 
-const DisplayComments = ({ comment, post, replies, comments, commentId }) => {
+const DisplayComments = ({ comment, post }) => {
   let rootPaddingLeft = 10;
   const [repliesForThisComment, setRepliesForThisComment] = useState([]);
-
   useEffect(() => {
-    if (!comment || !post || !replies || commentId) return;
-    setRepliesForThisComment(
-      replies.filter((each) => each.reply._id.toString() == comment._id.toString())
-    );
-  }, [comment, replies, post, commentId]);
+    if (!comment || !post) return;
+    setRepliesForThisComment(comment.replies ? comment.replies : []);
+  }, [comment, post]);
 
   return (
     <>
-      <Comment comment={comment} post={post} commentId={comment._id}>
+      <Comment comment={comment} post={post}>
         {/* children is its reply */}
         <div style={{ paddingLeft: `${rootPaddingLeft}px` }}>
-          {repliesForThisComment.map(
-            (item, index) =>
-              item.reply && (
-                <div>
-                  <Comment key={index} post={post} comment={item} commentId={comment._id} />
-                </div>
-              )
-          )}
+          {repliesForThisComment.length > 0 &&
+            repliesForThisComment.map((each, index) => {
+              return <DisplayComments key={index} comment={each} post={post} />;
+            })}
           {/* {replyCm.length - next > 0 ? (
             <div onClick={() => setNext(next + 10)}>See more replies...</div>
           ) : (
