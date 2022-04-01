89710fe4aa0f1d13848564ff54f02fa21e3d487b
jer_ng <coh.jr11@gmail.com>
2022-04-01 15:18:05 -0700

    frontend visual update 3

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,5 +1,5 @@
-import { useApolloClient, useMutation, useQuery } from '@apollo/client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useApolloClient, useMutation } from '@apollo/client';
 import { Mutation_Login } from '../graphql-client/mutations/login';
 import { useRouter } from 'next/router';
 import { Query_me } from '../graphql-client/queries/user';
@@ -9,7 +9,55 @@ import NextLink from 'next/link';
 import { Form, Formik } from 'formik';
 import InputField from '../components/InputField';
 import { LoadingButton } from '@mui/lab';
-import { Alert, Button, LinearProgress, Box } from '@mui/material';
+import { Grid, Alert, Button, LinearProgress, Box } from '@mui/material';
+import Image from 'next/image';
+import char1 from '../assets/redditChars1.jpg';
+import char2 from '../assets/redditChars2.jpg';
+import char3 from '../assets/redditChars3.jpg';
+import char4 from '../assets/redditChars4.jpg';
+import char5 from '../assets/redditChars5.jpg';
+import char6 from '../assets/redditChars6.jpg';
+import char7 from '../assets/redditChars7.jpg';
+import char8 from '../assets/redditChars8.jpg';
+
+// responsive
+import { styled } from '@mui/material/styles';
+export const ResponsiveBox = styled('div')(({ theme }) => ({
+  // mobile
+  [theme.breakpoints.down('md')]: {
+    margin: 0,
+    width: '100%',
+    height: '85vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '.hide_mobile': {
+      display: 'none',
+    },
+    '.grid_login': {
+      width: '100%',
+    },
+  },
+  // tablet
+  [theme.breakpoints.up('md')]: {
+    margin: '0 auto',
+    width: '100%',
+    maxHeight: '85vh',
+    height: '100%',
+    position: 'relative',
+    overflow: 'hidden',
+    '.decor_img_login': {},
+  },
+  // desktop
+  [theme.breakpoints.up('lg')]: {
+    margin: '0 auto',
+    width: '50%',
+    maxHeight: '85vh',
+    height: '100%',
+    position: 'relative',
+    overflow: 'hidden',
+  },
+}));
 
 const Login = () => {
   const client = useApolloClient();
@@ -21,6 +69,14 @@ const Login = () => {
   const [login, { loading: loginLoading, error }] = useMutation(Mutation_Login);
   const meData = client.readQuery({ query: Query_me });
   const [exceptionErr, setExceptionError] = useState(null);
+
+  useEffect(() => {
+    if (!exceptionErr) return;
+    setTimeout(() => {
+      setExceptionError(null);
+    }, 3000);
+  }, [exceptionErr]);
+
   const handleSubmit = async (values, { setErrors }) => {
     await login({
       variables: {
@@ -28,7 +84,6 @@ const Login = () => {
         password: values.password,
       },
       update(cache, { data }) {
-        console.log(data);
         if (!data.login.network.success) {
           setExceptionError(
             data.login.network.errors && data.login.network.errors.length == 1
@@ -54,64 +109,249 @@ const Login = () => {
   }
   if (error) setExceptionError(error);
   return (
-    <div>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        {({ isSubmitting }) => (
-          <Form>
-            <Box sx={{ display: 'flex', alignItems: 'center', paddingLeft: '1em', gap: '1em' }}>
-              <InputField name='userNameOrEmail' label='User Name' type='text' />
-              <InputField name='password' label='Password' type='password' />
-              <LoadingButton
-                sx={{
-                  border: '2px solid black',
-                  background: 'orange',
-                  color: 'black',
-                  '&:hover': {
-                    border: '2px solid #164920',
-                    color: 'white',
-                    background: '#164920',
-                  },
-                }}
-                variant='outlined'
-                loading={isSubmitting && loginLoading}
-                type='submit'
-              >
-                Login
-              </LoadingButton>
-              or
-              <NextLink href='/register'>
+    <ResponsiveBox>
+      <Grid
+        container
+        sx={{
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Grid
+          item
+          sx={{
+            width: '33%',
+            height: '25%',
+            objectFit: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Image src={char1.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          className='decor_img_login'
+          item
+          sx={{
+            width: '34%',
+            height: '25%',
+            objectFit: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Image src={char2.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            width: '33%',
+            height: '25%',
+            objectFit: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Image src={char3.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          className='hide_mobile'
+          item
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '20%',
+            height: '50%',
+            alignItems: 'center',
+            objectFit: 'cover',
+          }}
+        >
+          <Box
+            sx={{
+              height: 'fit-content',
+            }}
+          >
+            <Image src={char4.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          className='grid_login'
+          item
+          sx={{
+            width: '60%',
+            height: '50%',
+            overflow: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Box
+            sx={{
+              width: '100%',
+            }}
+          >
+            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+              {({ isSubmitting }) => (
+                <Form>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      gap: '1em',
+                      width: '100%',
+                    }}
+                  >
+                    <Box
+                      sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '1em' }}
+                    >
+                      <InputField name='userNameOrEmail' label='User Name' type='text' />
+                      <InputField name='password' label='Password' type='password' />
+                    </Box>
+                    {exceptionErr && <Alert severity='error'>{exceptionErr}</Alert>}
+                    <Box sx={{ display: 'flex', gap: '.5em', alignItems: 'center' }}>
+                      <LoadingButton
+                        sx={{
+                          border: '2px solid black',
+                          background: 'orange',
+                          color: 'black',
+                          '&:hover': {
+                            border: '2px solid #164920',
+                            color: 'white',
+                            background: '#164920',
+                          },
+                        }}
+                        variant='outlined'
+                        loading={isSubmitting && loginLoading}
+                        type='submit'
+                      >
+                        Login
+                      </LoadingButton>
+                      or
+                      <NextLink href='/register'>
+                        <Button
+                          sx={{
+                            border: '2px solid orange',
+                            color: 'orange',
+                            background: 'black',
+                            '&:hover': {
+                              border: '2px solid #164920',
+                              background: '#164920',
+                              color: 'white',
+                            },
+                          }}
+                          variant='contained'
+                        >
+                          Register
+                        </Button>
+                      </NextLink>
+                    </Box>
+                  </Box>
+                  {isSubmitting && loginLoading && <LinearProgress />}
+                </Form>
+              )}
+            </Formik>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                justifyContent: 'flex-end',
+              }}
+            >
+              <NextLink href='/account/forgotPassword'>
                 <Button
                   sx={{
-                    border: '2px solid orange',
-                    color: 'orange',
-                    background: 'black',
-                    '&:hover': {
-                      border: '2px solid #164920',
-                      background: '#164920',
-                      color: 'white',
-                    },
+                    width: 'fit-content',
+                    margin: '1em',
+                    textDecoration: 'underline',
+                    textTransform: 'lowercase',
                   }}
-                  variant='contained'
                 >
-                  Register
+                  Forgot password
                 </Button>
               </NextLink>
             </Box>
-            {exceptionErr && (
-              <Alert variant='filled' severity='error'>
-                {exceptionErr}
-              </Alert>
-            )}
-            {isSubmitting && loginLoading && <LinearProgress />}
-          </Form>
-        )}
-      </Formik>
-      <NextLink href='/account/forgotPassword'>
-        <Button sx={{ margin: '1em', textDecoration: 'underline', textTransform: 'lowercase' }}>
-          Forgot password
-        </Button>
-      </NextLink>
-    </div>
+          </Box>
+        </Grid>
+        <Grid
+          className='hide_mobile'
+          item
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            width: '20%',
+            alignItems: 'center',
+            objectFit: 'cover',
+          }}
+        >
+          <Box
+            sx={{
+              height: 'fit-content',
+            }}
+          >
+            <Image src={char5.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '33%',
+            height: '25%',
+            objectFit: 'cover',
+          }}
+        >
+          <Box>
+            <Image src={char6.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            width: '34%',
+            height: '25%',
+            objectFit: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Image src={char7.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            width: '33%',
+            height: '25%',
+            objectFit: 'cover',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Box>
+            <Image src={char8.src} width={'150em'} height={'150em'} />
+          </Box>
+        </Grid>
+      </Grid>
+    </ResponsiveBox>
   );
 };
 
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,5 +1,5 @@
 import { useApolloClient, useMutation, useQuery } from '@apollo/client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Mutation_register } from '../graphql-client/mutations/register';
 import { useRouter } from 'next/router';
 import { Query_me } from '../graphql-client/queries/user';
@@ -9,7 +9,17 @@ import NextLink from 'next/link';
 import { Form, Formik } from 'formik';
 import InputField from '../components/InputField';
 import { LoadingButton } from '@mui/lab';
-import { Alert, Button, LinearProgress, Box } from '@mui/material';
+import { Alert, Button, LinearProgress, Box, Grid } from '@mui/material';
+import { ResponsiveBox } from './login';
+import Image from 'next/image';
+import char1 from '../assets/redditChars1.jpg';
+import char2 from '../assets/redditChars2.jpg';
+import char3 from '../assets/redditChars3.jpg';
+import char4 from '../assets/redditChars4.jpg';
+import char5 from '../assets/redditChars5.jpg';
+import char6 from '../assets/redditChars6.jpg';
+import char7 from '../assets/redditChars7.jpg';
+import char8 from '../assets/redditChars8.jpg';
 
 const Register = () => {
   const client = useApolloClient();
@@ -19,9 +29,17 @@ const Register = () => {
     email: '',
     password: '',
   };
+
   const [register, { loading: registerLoading, error }] = useMutation(Mutation_register);
   const meData = client.readQuery({ query: Query_me });
   const [exceptionErr, setExceptionError] = useState(null);
+
+  useEffect(() => {
+    if (!exceptionErr) return;
+    setTimeout(() => {
+      setExceptionError(null);
+    }, 3000);
+  }, [exceptionErr]);
 
   const handleSubmit = async (values, { setErrors }) => {
     try {
@@ -57,32 +75,230 @@ const Register = () => {
   }
   if (error) setExceptionError(error);
   return (
-    <Box>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-        {({ isSubmitting }) => (
-          <Form>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: '1em', paddingLeft: '1em' }}>
-              <InputField name='userName' label='User Name' type='text' />
-              <InputField name='email' label='Email' type='text' />
-              <InputField name='password' label='Password' type='password' />
-              <LoadingButton
-                variant='contained'
-                loading={isSubmitting && registerLoading}
-                type='submit'
-              >
-                Register
-              </LoadingButton>
-              or
-              <NextLink href='/login'>
-                <Button variant='outlined'>Login</Button>
-              </NextLink>
-              {exceptionErr && <Alert severity='error'>{exceptionErr}</Alert>}
+    <>
+      <ResponsiveBox>
+        <Grid
+          container
+          sx={{
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Grid
+            item
+            sx={{
+              width: '33%',
+              height: '25%',
+              objectFit: 'cover',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Box>
+              <Image src={char1.src} width={'150em'} height={'150em'} />
             </Box>
-            {isSubmitting && registerLoading && <LinearProgress />}
-          </Form>
-        )}
-      </Formik>
-    </Box>
+          </Grid>
+          <Grid
+            className='decor_img_login'
+            item
+            sx={{
+              width: '34%',
+              height: '25%',
+              objectFit: 'cover',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Box>
+              <Image src={char2.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            sx={{
+              width: '33%',
+              height: '25%',
+              objectFit: 'cover',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Box>
+              <Image src={char3.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+          <Grid
+            className='hide_mobile'
+            item
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: '20%',
+              height: '50%',
+              alignItems: 'center',
+              objectFit: 'cover',
+            }}
+          >
+            <Box
+              sx={{
+                height: 'fit-content',
+              }}
+            >
+              <Image src={char4.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+          <Grid
+            className='grid_login'
+            item
+            sx={{
+              width: '60%',
+              height: '50%',
+              overflow: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Box
+              sx={{
+                width: '100%',
+              }}
+            >
+              <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                {({ isSubmitting }) => (
+                  <Form>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        gap: '1em',
+                        width: '100%',
+                      }}
+                    >
+                      <Box
+                        sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '1em' }}
+                      >
+                        <InputField name='userName' label='User Name' type='text' />
+                        <InputField name='email' label='Email' type='text' />
+                        <InputField name='password' label='Password' type='password' />
+                      </Box>
+                      {exceptionErr && <Alert severity='error'>{exceptionErr}</Alert>}
+                      <Box sx={{ display: 'flex', gap: '.5em', alignItems: 'center' }}>
+                        <LoadingButton
+                          sx={{
+                            border: '2px solid orange',
+                            color: 'orange',
+                            background: 'black',
+                            '&:hover': {
+                              border: '2px solid #164920',
+                              background: '#164920',
+                              color: 'white',
+                            },
+                          }}
+                          variant='contained'
+                          loading={isSubmitting && registerLoading}
+                          type='submit'
+                        >
+                          Register
+                        </LoadingButton>
+                        or
+                        <NextLink href='/login'>
+                          <Button
+                            sx={{
+                              border: '2px solid black',
+                              background: 'orange',
+                              color: 'black',
+                              '&:hover': {
+                                border: '2px solid #164920',
+                                color: 'white',
+                                background: '#164920',
+                              },
+                            }}
+                          >
+                            Login
+                          </Button>
+                        </NextLink>
+                      </Box>
+                    </Box>
+                    {isSubmitting && registerLoading && <LinearProgress />}
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          </Grid>
+          <Grid
+            className='hide_mobile'
+            item
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: '20%',
+              alignItems: 'center',
+              objectFit: 'cover',
+            }}
+          >
+            <Box
+              sx={{
+                height: 'fit-content',
+              }}
+            >
+              <Image src={char5.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '33%',
+              height: '25%',
+              objectFit: 'cover',
+            }}
+          >
+            <Box>
+              <Image src={char6.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            sx={{
+              width: '34%',
+              height: '25%',
+              objectFit: 'cover',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Box>
+              <Image src={char7.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            sx={{
+              width: '33%',
+              height: '25%',
+              objectFit: 'cover',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Box>
+              <Image src={char8.src} width={'150em'} height={'150em'} />
+            </Box>
+          </Grid>
+        </Grid>
+      </ResponsiveBox>
+    </>
   );
 };
 
