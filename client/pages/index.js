8ee56b9d27754b9feceefcacc28fae50527d994a
jer_ng <coh.jr11@gmail.com>
2022-04-26 11:01:08 -0700

    getStaticProps test

--- client/pages/index.js
+++ client/pages/index.js
@@ -11,14 +11,13 @@ export default function Home() {
     </>
   );
 }
-// export async function getStaticProps(context) {
-//   const apolloClient = initializeApollo({ headers: context?.req?.headers });
-//   await apolloClient.query({
-//     query: Query_getPosts,
-//     variables: { limit: 10 },
-//   });
-
-//   return addApolloState(apolloClient, {
-//     props: {},
-//   });
-// }
+export async function getStaticProps(context) {
+  const apolloClient = initializeApollo({ headers: context?.req?.headers });
+  await apolloClient.query({
+    query: Query_getPosts,
+    variables: { limit: 10 },
+  });
+  return addApolloState(apolloClient, {
+    props: {},
+  });
+}
